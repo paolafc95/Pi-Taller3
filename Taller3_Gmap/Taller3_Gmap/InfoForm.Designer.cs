58caa311e3b8bc8f29fb7890bc09f8edc7646f9d
Juan Manuel Imbachi Guengue <juan.imbachi@correo.icesi.edu.co>
2018-08-19 15:11:10 -0500

    Actualización visualización información de los grupos

--- Taller3_Gmap/Taller3_Gmap/InfoForm.Designer.cs
+++ Taller3_Gmap/Taller3_Gmap/InfoForm.Designer.cs
@@ -29,30 +29,15 @@
         private void InitializeComponent()
         {
             this.btnVolver = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.TxtBxNombreAconocimiento = new System.Windows.Forms.TextBox();
-            this.TxtBxNombreArea = new System.Windows.Forms.TextBox();
-            this.RegionTxtBx = new System.Windows.Forms.TextBox();
-            this.DptoATxtBx = new System.Windows.Forms.TextBox();
-            this.MunicipioATxtBx = new System.Windows.Forms.TextBox();
-            this.FechaCreaciónTxtBx = new System.Windows.Forms.TextBox();
-            this.NombreTxtBx = new System.Windows.Forms.TextBox();
-            this.codigoTxtBx = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1.SuspendLayout();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnVolver
             // 
+            this.btnVolver.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnVolver.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVolver.Location = new System.Drawing.Point(556, 342);
+            this.btnVolver.Location = new System.Drawing.Point(615, 6);
             this.btnVolver.Name = "btnVolver";
             this.btnVolver.Size = new System.Drawing.Size(84, 30);
             this.btnVolver.TabIndex = 16;
@@ -60,209 +45,36 @@
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
-            // panel1
+            // dataGridView1
             // 
-            this.panel1.Controls.Add(this.TxtBxNombreAconocimiento);
-            this.panel1.Controls.Add(this.TxtBxNombreArea);
-            this.panel1.Controls.Add(this.MunicipioATxtBx);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.RegionTxtBx);
-            this.panel1.Controls.Add(this.DptoATxtBx);
-            this.panel1.Controls.Add(this.FechaCreaciónTxtBx);
-            this.panel1.Controls.Add(this.NombreTxtBx);
-            this.panel1.Controls.Add(this.codigoTxtBx);
-            this.panel1.Controls.Add(this.label8);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(12, 35);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(628, 301);
-            this.panel1.TabIndex = 17;
-            // 
-            // TxtBxNombreAconocimiento
-            // 
-            this.TxtBxNombreAconocimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtBxNombreAconocimiento.Location = new System.Drawing.Point(253, 247);
-            this.TxtBxNombreAconocimiento.Name = "TxtBxNombreAconocimiento";
-            this.TxtBxNombreAconocimiento.Size = new System.Drawing.Size(238, 24);
-            this.TxtBxNombreAconocimiento.TabIndex = 31;
-            // 
-            // TxtBxNombreArea
-            // 
-            this.TxtBxNombreArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtBxNombreArea.Location = new System.Drawing.Point(124, 215);
-            this.TxtBxNombreArea.Name = "TxtBxNombreArea";
-            this.TxtBxNombreArea.Size = new System.Drawing.Size(233, 24);
-            this.TxtBxNombreArea.TabIndex = 30;
-            // 
-            // RegionTxtBx
-            // 
-            this.RegionTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RegionTxtBx.Location = new System.Drawing.Point(77, 166);
-            this.RegionTxtBx.Name = "RegionTxtBx";
-            this.RegionTxtBx.Size = new System.Drawing.Size(191, 24);
-            this.RegionTxtBx.TabIndex = 29;
-            // 
-            // DptoATxtBx
-            // 
-            this.DptoATxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DptoATxtBx.Location = new System.Drawing.Point(205, 98);
-            this.DptoATxtBx.Name = "DptoATxtBx";
-            this.DptoATxtBx.Size = new System.Drawing.Size(199, 24);
-            this.DptoATxtBx.TabIndex = 28;
-            // 
-            // MunicipioATxtBx
-            // 
-            this.MunicipioATxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MunicipioATxtBx.Location = new System.Drawing.Point(161, 133);
-            this.MunicipioATxtBx.Name = "MunicipioATxtBx";
-            this.MunicipioATxtBx.Size = new System.Drawing.Size(196, 24);
-            this.MunicipioATxtBx.TabIndex = 27;
-            // 
-            // FechaCreaciónTxtBx
-            // 
-            this.FechaCreaciónTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FechaCreaciónTxtBx.Location = new System.Drawing.Point(161, 49);
-            this.FechaCreaciónTxtBx.Name = "FechaCreaciónTxtBx";
-            this.FechaCreaciónTxtBx.Size = new System.Drawing.Size(196, 24);
-            this.FechaCreaciónTxtBx.TabIndex = 26;
-            // 
-            // NombreTxtBx
-            // 
-            this.NombreTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.NombreTxtBx.Location = new System.Drawing.Point(90, 15);
-            this.NombreTxtBx.Name = "NombreTxtBx";
-            this.NombreTxtBx.Size = new System.Drawing.Size(214, 22);
-            this.NombreTxtBx.TabIndex = 25;
-            // 
-            // codigoTxtBx
-            // 
-            this.codigoTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.codigoTxtBx.Location = new System.Drawing.Point(426, 11);
-            this.codigoTxtBx.Name = "codigoTxtBx";
-            this.codigoTxtBx.Size = new System.Drawing.Size(121, 22);
-            this.codigoTxtBx.TabIndex = 24;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(12, 250);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(234, 18);
-            this.label8.TabIndex = 23;
-            this.label8.Text = "Nombre área de conocimiento";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(12, 217);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(106, 18);
-            this.label7.TabIndex = 22;
-            this.label7.Text = "Nombre área";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(12, 169);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 18);
-            this.label6.TabIndex = 21;
-            this.label6.Text = "Región";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 104);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(187, 18);
-            this.label5.TabIndex = 20;
-            this.label5.Text = "Departamento asociado";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 136);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(146, 18);
-            this.label4.TabIndex = 19;
-            this.label4.Text = "Municipio asociado";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 52);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(142, 18);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "Fecha de creación";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(16, 15);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 18);
-            this.label2.TabIndex = 17;
-            this.label2.Text = "Nombre";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(360, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 18);
-            this.label1.TabIndex = 16;
-            this.label1.Text = "Código";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(687, 199);
+            this.dataGridView1.TabIndex = 17;
             // 
             // InfoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnVolver;
-            this.ClientSize = new System.Drawing.Size(652, 384);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(711, 255);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnVolver);
             this.Name = "InfoForm";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Información grupo de investigación";
+            this.Text = "Información Grupo de Investigación";
             this.Load += new System.EventHandler(this.InfoForm_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnVolver;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox TxtBxNombreAconocimiento;
-        private System.Windows.Forms.TextBox TxtBxNombreArea;
-        private System.Windows.Forms.TextBox MunicipioATxtBx;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox RegionTxtBx;
-        private System.Windows.Forms.TextBox DptoATxtBx;
-        private System.Windows.Forms.TextBox FechaCreaciónTxtBx;
-        private System.Windows.Forms.TextBox NombreTxtBx;
-        private System.Windows.Forms.TextBox codigoTxtBx;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
