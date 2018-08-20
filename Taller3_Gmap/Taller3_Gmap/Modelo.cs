dec13e4450319ebf8045587448877d6500591e75
Wilmer Andres Gomez Moreno <wilmer.gomez@correo.icesi.edu.co>
2018-08-19 19:06:10 -0500

    Cambios de posicion

--- Taller3_Gmap/Taller3_Gmap/Modelo.cs
+++ Taller3_Gmap/Taller3_Gmap/Modelo.cs
@@ -10,7 +10,7 @@ namespace Taller3_Gmap
     {
         public String temp = "";
         public String temp2 = "";
-        public int[] posDatos = { 3, 2, 4, 6, 5, 8, 11, 12 };
+        public int[] posDatos = { 2, 3, 4, 6, 5, 8, 11, 12 };
         private List<String> grupos  = new List<String>();
         public Modelo()
         {
@@ -40,7 +40,7 @@ namespace Taller3_Gmap
                 using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"./GRUPOS_DE_INVES.csv", true))
                 {
                     String[] datosGrupo = GrupoNuevo.Split(',');
-                    file.WriteLine(" , ," + datosGrupo[1] + "," + datosGrupo[0] + "," + datosGrupo[2] + "," + datosGrupo[4] + "," + datosGrupo[3] + ", ," + datosGrupo[5] + ", , ," + datosGrupo[6] + "," + datosGrupo[7] + ", , , ");
+                    file.WriteLine(" , ," + datosGrupo[0] + "," + datosGrupo[1] + "," + datosGrupo[2] + "," + datosGrupo[4] + "," + datosGrupo[3] + ", ," + datosGrupo[5] + ", , ," + datosGrupo[6] + "," + datosGrupo[7] + ", , , ");
                 }
             }
             else
@@ -63,7 +63,7 @@ namespace Taller3_Gmap
                     {
                         using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"./datos.txt", true))
                         {
-                            file.WriteLine(substrings[0]+","+ substrings[1]+"," + datosGrupo[1] + "," + datosGrupo[0] + "," + datosGrupo[2] + "," + datosGrupo[4] + "," + datosGrupo[3] + ","+ substrings[7]+"," + datosGrupo[5] + ","+ substrings[9]+ ","+ substrings[10]+"," + datosGrupo[6] + "," + datosGrupo[7] + ","+ substrings[13]+","+ substrings[14]+ ","+ substrings[5]);
+                            file.WriteLine(substrings[0]+","+ substrings[1]+"," + datosGrupo[0] + "," + datosGrupo[1] + "," + datosGrupo[2] + "," + datosGrupo[4] + "," + datosGrupo[3] + ","+ substrings[7]+"," + datosGrupo[5] + ","+ substrings[9]+ ","+ substrings[10]+"," + datosGrupo[6] + "," + datosGrupo[7] + ","+ substrings[13]+","+ substrings[14]+ ","+ substrings[5]);
                         }
                     }
 
